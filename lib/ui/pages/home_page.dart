612c77f4d26c4772ae8803e6845b15a67477b241
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2023-07-28 23:23:37 -0300

    Fix search

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -79,37 +79,46 @@ class HomePage extends StatelessWidget {
               const SizedBox(
                 height: 8.0,
               ),
-              Expanded(
-                child: SizedBox(
-                  height: size.height * 0.225,
-                  child: ListView.separated(
-                    itemCount: controller.nameSearch.text != ''
-                        ? controller.productsSearch.length
-                        : controller.products.length,
-                    separatorBuilder: (context, index) {
-                      return const Divider(
-                        height: 2,
-                        thickness: 1,
-                        color: Color(0xFFF0F0F0),
-                      );
-                    },
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: () => Navigator.pushNamed(
-                          context,
-                          '/details',
-                          arguments: controller.products[index],
+              (controller.nameSearch.text != '' &&
+                      controller.productsSearch.isEmpty)
+                  ? Center(
+                      child: SizedBox(
+                        width: size.width * 0.45,
+                        height: size.height * 0.3,
+                        child: Image.asset('assets/images/empty_list.png'),
+                      ),
+                    )
+                  : Expanded(
+                      child: SizedBox(
+                        height: size.height * 0.225,
+                        child: ListView.separated(
+                          itemCount: controller.nameSearch.text != ''
+                              ? controller.productsSearch.length
+                              : controller.products.length,
+                          separatorBuilder: (context, index) {
+                            return const Divider(
+                              height: 2,
+                              thickness: 1,
+                              color: Color(0xFFF0F0F0),
+                            );
+                          },
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () => Navigator.pushNamed(
+                                context,
+                                '/details',
+                                arguments: controller.products[index],
+                              ),
+                              child: ProductItem(
+                                product: controller.nameSearch.text != ''
+                                    ? controller.productsSearch[index]
+                                    : controller.products[index],
+                              ),
+                            );
+                          },
                         ),
-                        child: ProductItem(
-                          product: controller.nameSearch.text != ''
-                              ? controller.productsSearch[index]
-                              : controller.products[index],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
+                      ),
+                    ),
             ],
           );
         },
